5d132792cd2e2247e0bac86c0053f96d6f0486b9
Xiaosu Zhu <xiaosu.zhu@outlook.com>
2021-01-06 22:09:39 +0800

    test code with pure encoder-decoder

--- src/mcqc/models/quantizer.py
+++ src/mcqc/models/quantizer.py
@@ -1,3 +1,5 @@
+from typing import Callable
+
 import torch
 from torch import nn
 import torch.nn.functional as F
@@ -8,24 +10,24 @@ from mcqc.layers.gumbelSoftmax import GumbelSoftmax
 
 
 class _resBlock(nn.Module):
-    def __init__(self, d: int, rate: float = 0.1):
+    def __init__(self, d: int, rate: float = 0.1, activationFn: Callable = None):
         super().__init__()
-        self._layers = nn.Sequential(LayerGroup(d, d, rate), LayerGroup(d, 2 * d, rate), LayerGroup(2 * d, d, rate))
+        self._layers = nn.Sequential(LayerGroup(d, d, rate, activationFn), LayerGroup(d, 2 * d, rate, activationFn), LayerGroup(2 * d, d, rate, activationFn))
 
     def forward(self, x):
         return x + self._layers(x)
 
 
 class _incResBlock(nn.Module):
-    def __init__(self, d: int, rate: float = 0.1):
+    def __init__(self, d: int, rate: float = 0.1, activationFn: Callable = None):
         super().__init__()
-        self._fc1 = LayerGroup(d, d // 2, rate)
-        self._fc2 = LayerGroup(d // 2, d // 4, rate)
-        self._fc3 = LayerGroup(d // 4, d // 2, rate)
-        self._fc4 = LayerGroup(d // 2, d, rate)
-        self._res1 = _resBlock(d // 4, rate)
-        self._res2 = _resBlock(d // 2, rate)
-        self._res3 = _resBlock(d, rate)
+        self._fc1 = LayerGroup(d, d // 2, rate, activationFn)
+        self._fc2 = LayerGroup(d // 2, d // 4, rate, activationFn)
+        self._fc3 = LayerGroup(d // 4, d // 2, rate, activationFn)
+        self._fc4 = LayerGroup(d // 2, d, rate, activationFn)
+        self._res1 = _resBlock(d // 4, rate, activationFn)
+        self._res2 = _resBlock(d // 2, rate, activationFn)
+        self._res3 = _resBlock(d, rate, activationFn)
 
     def forward(self, x):
         # d // 2
@@ -41,14 +43,13 @@ class _incResBlock(nn.Module):
         return x + x3 + self._fc4(self._fc3(x2) + x1)
 
 
-class Quantizer(Module):
+class Quantizer(nn.Module):
     def __init__(self, k: int, cin: int, rate: float = 0.1):
         super().__init__()
         self._net = nn.Linear(cin, k)
         self._codebook = nn.Parameter(torch.randn(k, cin))
 
-    @Module.register("forward")
-    def _ff(self, x, temperature, hard):
+    def forward(self, x, temperature, hard):
         x = x.permute(0, 2, 3, 1)
         # [N, h, w, k]
         logits = self._net(x)
@@ -57,10 +58,10 @@ class Quantizer(Module):
         quantized = samples @ self._codebook
         return quantized.permute(0, 3, 1, 2), samples, logits.permute(0, 3, 1, 2)
 
-    @Module.register("quantize")
-    def _quantize(self, logits, temperature, hard):
-        logits = logits.permute(0, 2, 3, 1)
-        samples = F.gumbel_softmax(logits, temperature, hard)
-        # [N, h, w, C] <- [N, h, w, k] @ [k, C]
-        quantized = samples @ self._codebook
-        return quantized.permute(0, 3, 1, 2)
+    # @Module.register("quantize")
+    # def _quantize(self, logits, temperature, hard):
+    #     logits = logits.permute(0, 2, 3, 1)
+    #     samples = F.gumbel_softmax(logits, temperature, hard)
+    #     # [N, h, w, C] <- [N, h, w, k] @ [k, C]
+    #     quantized = samples @ self._codebook
+    #     return quantized.permute(0, 3, 1, 2)
