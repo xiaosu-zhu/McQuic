ea5866d4c493e6ee8d7a2d5adc0524a6723e0c33
Xiaosu Zhu <xiaosu.zhu@outlook.com>
2021-03-22 17:14:18 +0800

    prepare switch to ddp

--- src/mcqc/algorithms/twoStage.py
+++ src/mcqc/algorithms/twoStage.py
@@ -5,7 +5,8 @@ import math
 import numpy as np
 import torch
 from torch import nn
-from torch.distributions import Categorical
+from torch.nn.parallel import DistributedDataParallel
+from torch import distributed as dist
 from cfmUtils.saver import Saver
 from cfmUtils.vision.colorSpace import rgb2hsv, hsv2rgb
 from pytorch_msssim import ms_ssim
@@ -26,22 +27,20 @@ def _transformerLR(epoch):
 
 
 class TwoStage(Algorithm):
-    def __init__(self, config: Config, model: Whole, device: str, optimizer: Callable[[Iterator[nn.Parameter]], torch.optim.Optimizer], scheduler: Callable[[torch.optim.Optimizer], torch.optim.lr_scheduler._LRScheduler], saver: Saver, continueTrain: bool, logger: Logger):
+    def __init__(self, rank: int, worldSize: int, config: Config, model: Whole, device: str, optimizer: Callable[[Iterator[nn.Parameter]], torch.optim.Optimizer], scheduler: Callable[[torch.optim.Optimizer], torch.optim.lr_scheduler._LRScheduler], saver: Saver, continueTrain: bool, logger: Logger):
         super().__init__()
-        self._model = model
-        if device == "cuda" and torch.cuda.device_count() > 1:
-            self._model = nn.DataParallel(self._model.to(device))
-        else:
-            self._model = self._model.to(device)
+        self._model = DistributedDataParallel(model.to(rank))
         self._device = device
 
         # self._optimizer = torch.optim.AdamW(optimizer_grouped_parameters, eps=Consts.Eps, amsgrad=True)
-        self._optimizer = optimizer(config.LearningRate, self._model.parameters(), 0)
+        self._optimizer = optimizer(config.LearningRate, self._model.parameters(), 1e-5)
         self._scheduler = scheduler(self._optimizer)
         # self._scheduler = torch.optim.lr_scheduler.LambdaLR(self._optimizer, _transformerLR)
 
         # self._optimizerD = optimizer(1e-5, self._model.module._discriminator.parameters(), 0)
         # self._schedulerD = scheduler(self._optimizerD)
+        self._rank = rank
+        self._worldSize = worldSize
         self._saver = saver
         self._logger = logger
         self._config = config
@@ -65,7 +64,7 @@ class TwoStage(Algorithm):
         maxCV = 0.1
 
         if self._continue:
-            loaded = self._saver.load(self._saver.SavePath, self._logger, model=self._model)# , optimG=self._optimizerG, schdrG=self._schedulerG, step=step, temp=initTemp)
+            loaded = Saver.load(self._saver.SavePath, self._logger, model=self._model)# , optimG=self._optimizerG, schdrG=self._schedulerG, step=step, temp=initTemp)
             # initTemp = loaded["temp"]
             # step = loaded["step"]
 
@@ -80,7 +79,7 @@ class TwoStage(Algorithm):
                 # if (step + 1) % self._accumulatedBatches == 0:
                 #     self._optimizer.step()
                 #     self._optimizer.zero_grad()
-                (self._config.Coef.ssim * ssimLoss + self._config.Coef.l1l2 * l1l2Loss + self._config.Coef.l1l2 * qLoss + self._config.Coef.reg * reg).mean().backward()
+                (self._config.Coef.ssim * ssimLoss + self._config.Coef.l1l2 * l1l2Loss + 10 * self._config.Coef.l1l2 * qLoss + self._config.Coef.reg * reg).mean().backward()
                 # (self._config.Coef.l1l2 * l1l2Loss + self._config.Coef.reg * reg).mean().backward()
                 # (self._config.Coef.ssim * ssimLoss + self._config.Coef.reg * reg).mean().backward()
                 # torch.nn.utils.clip_grad_norm_(self._model.parameters(), max_norm=0.5)
@@ -90,10 +89,13 @@ class TwoStage(Algorithm):
                 # self._scheduler.step()
                 with torch.no_grad():
                     # cv = 10 ** float(l1l2Loss.mean() * -10)
-                    self._saver.add_scalar("loss/ssimLoss", ssimLoss.mean(), global_step=step)
-                    self._saver.add_scalar("loss/l1l2Loss", l1l2Loss.mean(), global_step=step)
-                    self._saver.add_scalar("loss/qLoss", qLoss.mean(), global_step=step)
-                    self._saver.add_scalar("loss/reg", reg.mean(), global_step=step)
+                    if self._saver is not None:
+                        # group = dist.new_group(list(range(self._worldSize)))
+                        # dist.gather(ssimLoss,)
+                        self._saver.add_scalar("loss/ssimLoss", ssimLoss.mean(), global_step=step)
+                        self._saver.add_scalar("loss/l1l2Loss", l1l2Loss.mean(), global_step=step)
+                        self._saver.add_scalar("loss/qLoss", qLoss.mean(), global_step=step)
+                        self._saver.add_scalar("loss/reg", reg.mean(), global_step=step)
                     if (step + 1) % 1000 == 0:
                         self._saver.add_images("train/raw", self._deTrans(images), global_step=step)
                         self._saver.add_images("train/res", self._deTrans(restored), global_step=step)
