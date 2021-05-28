225767b801b043d80d56ee55667f144742f1e690
Xiaosu Zhu <xiaosu.zhu@outlook.com>
2021-05-28 13:40:10 +0800

    update hyper-params

--- src/mcqc/Consts.py
+++ src/mcqc/Consts.py
@@ -17,4 +17,4 @@ class Consts:
     DataDir = os.path.join(srcRoot, "../data")
     SaveDir = os.path.join(srcRoot, "../saved")
     Logger = logging.getLogger(LoggerName)
-    Eps = 1e-8
+    Eps = 1e-12
