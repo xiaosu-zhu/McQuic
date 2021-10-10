35424f49494562dcd6c130f5a245af14ae17db6a
Xiaosu Zhu <xiaosu.zhu@outlook.com>
2021-10-10 19:47:37 +0800

    update code

--- src/mcqc/algorithms/new.py
+++ src/mcqc/algorithms/new.py
@@ -29,9 +29,9 @@ from mcqc.utils.vision import getTrainingFullTransform, getTrainingPreprocess
 
 
 _logMapping = {
-    "distortion": "Stat/Distortion",
-    "auxiliary": "Stat/Auxiliary",
-    "predict": "Stat/Context",
+    "distortion": "Loss/Distortion",
+    "auxiliary": "Loss/Auxiliary",
+    "predict": "Loss/Context",
     "bpp": "Stat/Reg",
     "lr": "Stat/LR",
     "regCoeff": "Stat/Reg",
@@ -88,11 +88,13 @@ class New(Algorithm):
     def _trainingStat(self, **kwArgs):
         step = kwArgs["now"]
         self._saver.add_scalar(_logMapping["distortion"], -10 * kwArgs["distortion"].log10(), global_step=step)
-        self._saver.add_scalar(_logMapping["auxiliary"], kwArgs["auxiliary"], global_step=step)
+        self._saver.add_scalar("Loss/Reg", kwArgs["auxiliary"][0], global_step=step)
+        self._saver.add_scalar("Loss/MLE", kwArgs["auxiliary"][1], global_step=step)
         # self._saver.add_scalar(_logMapping["predict"], kwArgs["predict"], global_step=step)
         # self._saver.add_scalar(_logMapping["bpp"], kwArgs["bpp"], global_step=step)
         self._saver.add_scalar(_logMapping["lr"], self._scheduler.get_last_lr()[0], global_step=step)
-        # self._saver.add_scalar(_logMapping["regCoeff"], self._regScheduler.Value, global_step=step)
+        self._saver.add_scalar(_logMapping["regCoeff"], self._regScheduler.Value, global_step=step)
+        self._saver.add_scalar("Stat/MLE", kwArgs["mle"], global_step=step)
         # self._saver.add_scalar(_logMapping["temperature"], kwArgs["temperature"], global_step=step)
 
     @torch.inference_mode()
@@ -152,6 +154,8 @@ class New(Algorithm):
     def run(self, trainLoader: Prefetcher, sampler: DistributedSampler, evalLoader: DataLoader, testLoader: DataLoader):
         step = 0
 
+        mle = 0.0
+
         temperature = 1.0
         # finalTemp = 0.001 / math.sqrt(self._config.Model.k[0])
         # annealRate = 0.9
@@ -196,24 +200,25 @@ class New(Algorithm):
             # bppCoef = self._regScheduler.Value / (self._config.Coef.ssim + self._config.Coef.l1l2 + self._regScheduler.Value)
             for images in tqdm(trainLoader, ncols=40, bar_format="Epoch [%3d] {n_fmt}/{total_fmt} |{bar}|" % (i + 1), total=totalBatches, leave=False, disable=self._rank != 0):
                 self._optimizer.zero_grad()
-                dLoss, auxLoss, (restored, allHards, allLogits) = self._model(images, temperature)
-                (dLoss + 1e-3 * auxLoss).backward()
+                dLoss, (regLoss, mleLoss), (restored, allHards, allLogits) = self._model(images, temperature)
+                (dLoss + self._regScheduler.Value * regLoss + mle * mleLoss).backward()
                 # if True:
                 #     torch.nn.utils.clip_grad_norm_(self._model.parameters(), 0.5)
                 self._optimizer.step()
                 step += 1
                 # updateOp()
                 if step % 2 == 0 and self._loggingHook is not None:
-                    self._trainingStat(now=step, epoch=i, distortion=dLoss, auxiliary=auxLoss)
+                    self._trainingStat(now=step, epoch=i, distortion=dLoss, auxiliary=(regLoss, mleLoss), reg=self._regScheduler.Value, mle=mle)
                 dist.barrier()
             if self._loggingHook is not None:
                 self._loggingHook(i + 1, now=step, images=images, restored=restored, evalLoader=evalLoader, testLoader=testLoader, epoch=i + 1, temperature=temperature, logits=allLogits[0], codes=allHards)
-            if (i + 1) % (self._config.TestFreq * 10) == 0:
-                self._optimizer.zero_grad()
-                self._reSpreadAll()
+            # if (i + 1) % (self._config.TestFreq * 10) == 0:
+            #     self._optimizer.zero_grad()
+            #     self._reSpreadAll()
             if self._scheduler is not None:
                 self._scheduler.step()
             self._regScheduler.step()
+            mle = 1.e-4 * (1 + math.sin(math.pi * i / self._config.Epoch)) / 2
 
     def _reSpreadAll(self):
         if self._rank == 0:
