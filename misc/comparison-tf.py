fedf114fdbc7a01fe98a3ff0e8b000798d0b8d71
Xiaosu Zhu <xiaosu.zhu@outlook.com>
2021-10-28 11:11:53 +0800

    add tf speed test

--- misc/comparison-tf.py
+++ misc/comparison-tf.py
@@ -1,7 +1,11 @@
 import io
+import time
 import os
 import sys
 import urllib
+from urllib import request
+
+from tqdm import trange
 from absl import app
 from absl.flags import argparse_flags
 import numpy as np
@@ -14,6 +18,10 @@ URL_PREFIX = "https://storage.googleapis.com/tensorflow_compression/metagraphs"
 # Default location to store cached metagraphs.
 METAGRAPH_CACHE = "/tmp/tfc_metagraphs"
 
+
+balle_factorized = "bmshj2018-factorized-msssim-6"
+balle_hyperprior = "bmshj2018-hyperprior-msssim-6"
+minnen2018 = "mbt2018-mean-msssim-6"
 
 def read_png(filename):
   """Loads a PNG image file."""
@@ -32,73 +40,96 @@ def write_png(filename, image):
   string = tf.image.encode_png(image)
   tf.io.write_file(filename, string)
 
+
+def instantiate_model_signature(model, signature, inputs=None, outputs=None):
+  """Imports a trained model and returns one of its signatures as a function."""
+  string = load_cached(model + ".metagraph")
+  metagraph = tf.compat.v1.MetaGraphDef()
+  metagraph.ParseFromString(string)
+  wrapped_import = tf.compat.v1.wrap_function(
+      lambda: tf.compat.v1.train.import_meta_graph(metagraph), [])
+  graph = wrapped_import.graph
+  if inputs is None:
+    inputs = metagraph.signature_def[signature].inputs
+    inputs = [graph.as_graph_element(inputs[k].name) for k in sorted(inputs)]
+  else:
+    inputs = [graph.as_graph_element(t) for t in inputs]
+  if outputs is None:
+    outputs = metagraph.signature_def[signature].outputs
+    outputs = [graph.as_graph_element(outputs[k].name) for k in sorted(outputs)]
+  else:
+    outputs = [graph.as_graph_element(t) for t in outputs]
+  return wrapped_import.prune(inputs, outputs)
+
+
 def compress_image(model, input_image):
   """Compresses an image tensor into a bitstring."""
   sender = instantiate_model_signature(model, "sender")
-  tensors = sender(input_image)
+  # warm up
+  for i in range(5):
+    tensors = sender(input_image)
+  start = time.time()
+  for i in trange(500):
+    tensors = sender(input_image)
+  end = time.time()
+  print("encoder time in ms:", (end - start) / 500 * 1000)
   packed = tfc.PackedTensors()
   packed.model = model
   packed.pack(tensors)
   return packed.string
 
 
-def compress(model, input_file, output_file, target_bpp=None, bpp_strict=False):
+def load_cached(filename):
+  """Downloads and caches files from web storage."""
+  pathname = os.path.join(METAGRAPH_CACHE, filename)
+  try:
+    with tf.io.gfile.GFile(pathname, "rb") as f:
+      string = f.read()
+  except tf.errors.NotFoundError:
+    url = f"{URL_PREFIX}/{filename}"
+    request = urllib.request.urlopen(url)
+    try:
+      string = request.read()
+    finally:
+      request.close()
+    tf.io.gfile.makedirs(os.path.dirname(pathname))
+    with tf.io.gfile.GFile(pathname, "wb") as f:
+      f.write(string)
+  return string
+
+
+
+def compress(model, input_file, output_file):
   """Compresses a PNG file to a TFCI file."""
-  if not output_file:
-    output_file = input_file + ".tfci"
+#   output_file = input_file + ".tfci"
 
   # Load image.
   input_image = read_png(input_file)
   num_pixels = input_image.shape[-2] * input_image.shape[-3]
 
-  if not target_bpp:
-    # Just compress with a specific model.
-    bitstring = compress_image(model, input_image)
-  else:
-    # Get model list.
-    models = load_cached(model + ".models")
-    models = models.decode("ascii").split()
-
-    # Do a binary search over all RD points.
-    lower = -1
-    upper = len(models)
-    bpp = None
-    best_bitstring = None
-    best_bpp = None
-    while bpp != target_bpp and upper - lower > 1:
-      i = (upper + lower) // 2
-      bitstring = compress_image(models[i], input_image)
-      bpp = 8 * len(bitstring) / num_pixels
-      is_admissible = bpp <= target_bpp or not bpp_strict
-      is_better = (best_bpp is None or
-                   abs(bpp - target_bpp) < abs(best_bpp - target_bpp))
-      if is_admissible and is_better:
-        best_bitstring = bitstring
-        best_bpp = bpp
-      if bpp < target_bpp:
-        lower = i
-      if bpp > target_bpp:
-        upper = i
-    if best_bpp is None:
-      assert bpp_strict
-      raise RuntimeError(
-          "Could not compress image to less than {} bpp.".format(target_bpp))
-    bitstring = best_bitstring
-
+  # Just compress with a specific model.
+  bitstring = compress_image(model, input_image)
   # Write bitstring to disk.
   with tf.io.gfile.GFile(output_file, "wb") as f:
     f.write(bitstring)
 
 
-def decompress(input_file, output_file):
+def decompress(model, input_file, output_file):
   """Decompresses a TFCI file and writes a PNG file."""
   if not output_file:
     output_file = input_file + ".png"
   with tf.io.gfile.GFile(input_file, "rb") as f:
     packed = tfc.PackedTensors(f.read())
-  receiver = instantiate_model_signature(packed.model, "receiver")
+  receiver = instantiate_model_signature(model, "receiver")
   tensors = packed.unpack([t.dtype for t in receiver.inputs])
-  output_image, = receiver(*tensors)
+  # warm up
+  for i in range(5):
+    output_image, = receiver(*tensors)
+  start = time.time()
+  for i in trange(500):
+    output_image, = receiver(*tensors)
+  end = time.time()
+  print("decoder time in ms:", (end - start) / 500 * 1000)
   write_png(output_file, output_image)
 
 
@@ -159,24 +190,12 @@ def dump_tensor(model, tensors, input_file, output_file):
 def main(args):
   # Command line can override these defaults.
   global URL_PREFIX, METAGRAPH_CACHE
-  URL_PREFIX = args.url_prefix
-  METAGRAPH_CACHE = args.metagraph_cache
+#   URL_PREFIX = args.url_prefix
+#   METAGRAPH_CACHE = args.metagraph_cache
 
-  # Invoke subcommand.
-  if args.command == "compress":
-    compress(args.model, args.input_file, args.output_file,
-             args.target_bpp, args.bpp_strict)
-  elif args.command == "decompress":
-    decompress(args.input_file, args.output_file)
-  elif args.command == "models":
-    list_models()
-  elif args.command == "tensors":
-    list_tensors(args.model)
-  elif args.command == "dump":
-    if not args.tensor:
-      raise ValueError("Must provide at least one tensor to dump.")
-    dump_tensor(args.model, args.tensor, args.input_file, args.output_file)
+  compress(balle_hyperprior, "data/kodak/kodim01.png", "data/kodak/kodim01.tfci")
+  decompress(balle_hyperprior, "data/kodak/kodim01.tfci", "data/kodak/kodim01.png")
 
 
 if __name__ == "__main__":
-  app.run(main, flags_parser=parse_args)
+  app.run(main)
