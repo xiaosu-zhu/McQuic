62abb9a5456e16a1e07ed3ead0d0e43fc00c5c94
Xiaosu Zhu <xiaosu.zhu@outlook.com>
2022-01-11 17:49:24 +0800

    update logging

--- src/main.py
+++ src/main.py
@@ -13,7 +13,7 @@ FLAGS = flags.FLAGS
 
 flags.DEFINE_string("cfg", "", "The config.json path.")
 flags.DEFINE_string("path", "", "Specify saving path, otherwise use default pattern. In eval mode, you must specify this path where saved checkpoint exists.")
-flags.DEFINE_string("port", "29936", "MASTER_PORT of DistributedDataParallel.")
+flags.DEFINE_string("master_port", "29936", "MASTER_PORT of DistributedDataParallel.")
 flags.DEFINE_boolean("eval", False, "Evaluate performance. Must specify arg 'path', and arg 'config' will be ignored.")
 flags.DEFINE_boolean("r", False, "Be careful to set to true. Whether to continue last training (with current config).")
 flags.DEFINE_boolean("debug", False, "Set to true to logging verbosely and require lower gpu.")
@@ -47,7 +47,7 @@ def main(_):
         gpus = queryGPU(needGPUs=config.GPUs, wantsMore=config.WantsMore, needVRamEachGPU=(config.VRam + 256) if config.VRam > 0 else -1, writeOSEnv=True)
         worldSize = len(gpus)
         config.scaleByWorldSize(worldSize)
-        mp.spawn(train, (worldSize, FLAGS.port, config, saveDir, FLAGS.r, FLAGS.debug), worldSize) # type: ignore
+        mp.spawn(train, (worldSize, FLAGS.master_port, config, saveDir, FLAGS.r, FLAGS.debug), worldSize) # type: ignore
 
 if __name__ == "__main__":
     app.run(main)
