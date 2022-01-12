392d67b115ff6d1c0a1217f0811592f0224200af
Xiaosu Zhu <xiaosu.zhu@outlook.com>
2022-01-12 22:34:15 +0800

    udpate code

--- src/mcqc/utils/helper.py
+++ src/mcqc/utils/helper.py
@@ -1,9 +1,11 @@
 import os
+
+from rich.progress import BarColumn, Progress, SpinnerColumn, TimeElapsedColumn
 import torch
+from torch import nn
+import torch.distributed as dist
 import random
 import numpy as np
-import torch.distributed as dist
-
 
 def initializeProcessGroup(port: str, rank: int, worldSize: int):
     os.environ["MASTER_ADDR"] = "localhost"
@@ -15,3 +17,20 @@ def initializeProcessGroup(port: str, rank: int, worldSize: int):
     torch.cuda.set_device(rank)
     np.random.seed(3407)
     dist.init_process_group("nccl", world_size=worldSize, rank=rank)
+
+
+def getRichProgress():
+    return Progress("Epoch [{task.fields[epoch]:%4d}]: {task.completed:4d}/{task.total:4d}", SpinnerColumn(), BarColumn(), TimeElapsedColumn(), "D = {task.fields[loss]}", transient=True, disable=dist.get_rank() != 0)
+
+
+class EMATracker(nn.Module):
+    def __init__(self, size: torch.Size, momentum: float = 0.9):
+        super().__init__()
+        self._momentum = momentum
+        self.register_buffer("_shadow", torch.empty(size) * torch.nan)
+
+    def forward(self, x: torch.Tensor):
+        if torch.all(torch.isnan(self._shadow)):
+            return self._shadow.copy_(x)
+        self._shadow -= self._momentum * (self._shadow - x)
+        return self._shadow
