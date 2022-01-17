8e2792005b5f20863c8c3fd33807e4ba447bd516
Xiaosu Zhu <xiaosu.zhu@outlook.com>
2022-01-17 22:04:31 +0800

    fix bug

--- mcqc/validation/validator.py
+++ mcqc/validation/validator.py
@@ -21,14 +21,6 @@ class Validator:
             BPP().to(rank)
         ])
 
-        self._interrupted = False
-        self._lock = threading.Semaphore()
-
-    def interrupt(self):
-        self._interrupted = True
-        self._lock.acquire()
-        self._lock.release()
-
     def tensorToImage(self, x: torch.Tensor) -> torch.Tensor:
         return self._deTrans(x)
 
@@ -43,35 +35,27 @@ class Validator:
 
     @torch.inference_mode()
     def count(self, epoch: int, model: BaseCompressor, trainLoader: DataLoader, progress: Progress):
-        self._lock.acquire()
         total = len(trainLoader)
         now = 0
         task = progress.add_task(f"Stat@{epoch:4d}", total=total, progress=f"{now:4d}/{total:4d}", suffix="")
         for now, image in enumerate(trainLoader):
-            if self._interrupted:
-                break
             model.count(image.to(self._rank, non_blocking=True))
             progress.update(task, advance=1, progress=f"{(now + 1):4d}/{total:4d}")
         progress.remove_task(task)
-        self._lock.release()
 
     @torch.inference_mode()
     def validate(self, epoch: int, model: BaseCompressor, valLoader: DataLoader, progress: Progress):
-        self._lock.acquire()
         total = len(valLoader)
         now = 0
         task = progress.add_task(f"Val@{epoch:4d}", total=total, progress=f"{now:4d}/{total:4d}", suffix="")
         with model._quantizer.readyForCoding() as cdfs:
             for now, images in enumerate(valLoader):
-                if self._interrupted:
-                    break
                 images = images.to(self._rank, non_blocking=True)
                 codes, binaries, header = model.compress(images, cdfs)
                 restored = model.decompress(codes, binaries, cdfs, header)
                 self._meter(images=self.tensorToImage(images), binaries=binaries, restored=self.tensorToImage(restored))
                 progress.update(task, advance=1, progress=f"{(now + 1):4d}/{total:4d}")
         progress.remove_task(task)
-        self._lock.release()
         return self._meter.results(), self._meter.summary()
 
     def test(self, testLoader: DataLoader): raise NotImplementedError
