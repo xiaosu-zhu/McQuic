b385d4ff72155ee82eee9f18fecacbfeaaed40eb
Xiaosu Zhu <xiaosu.zhu@outlook.com>
2022-01-21 21:37:22 +0800

    remove algorithms

--- mcquic/algorithms/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Package of algorithms
-
-Exports:
-    ProximalPolicy: Original Proximal Policy Optimization algorithm.
-    VCProximalPolicy: Value-Corrected Proximal Policy Optimization algorithm.
-    SoftActorCritic: Soft Actor-Critic algorithm.
-"""
-from .new import New
-from .pixelCNN import PixelCNN
