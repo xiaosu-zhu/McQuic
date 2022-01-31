0b96b4cac56bfece9390aabd90d45650a4d25c2c
Xiaosu <xiaosu.zhu@outlook.com>
2022-01-31 17:57:10 +0800

    update resume logic

--- mcquic/train/__init__.py
+++ mcquic/train/__init__.py
@@ -1,5 +1,7 @@
+from shutil import copy2
 from typing import Tuple, Union
 import os
+from numpy import save
 
 import torch
 from torch import nn
@@ -28,7 +30,14 @@ def modelFn(channel, m, k, lossTarget) -> Tuple[BaseCompressor, nn.Module]:
     return compressor, criterion
 
 def train(rank: int, worldSize: int, port: str, config: Config, saveDir: str, resume: Union[str, None], debug: bool):
-    saver = getSaver(saveDir, saveName="saved.ckpt", loggerName=Consts.Name, loggingLevel="DEBUG" if debug else "INFO", config=config, reserve=resume is not None, disable=rank != 0)
+    # load ckpt before create trainer, in case it moved to other place.
+    if resume is not None and os.path.exists(resume) and resume.endswith("ckpt"):
+        tmpFile = copy2(resume, os.path.join(Consts.TempDir, "resume.ckpt"), follow_symlinks=False)
+        # stateDict = torch.load(resume, {"cuda:0": f"cuda:{rank}"})[Consts.Fingerprint]
+    else:
+        tmpFile = None
+
+    saver = getSaver(saveDir, saveName="saved.ckpt", loggerName=Consts.Name, loggingLevel="DEBUG" if debug else "INFO", config=config, reserve=False, disable=rank != 0, dumpFile=Consts.RootDir)
 
     saver.info("Here is the whole config during this run: \r\n%s", summary(config))
 
@@ -43,8 +52,9 @@ def train(rank: int, worldSize: int, port: str, config: Config, saveDir: str, re
 
     trainer = getTrainer(rank, config, lambda: modelFn(config.Model.channel, config.Model.m, config.Model.k, config.Model.target), optimizerFn, schdrFn, valueTunerFns, saver)
 
-    if resume is not None and os.path.exists(resume) and resume.endswith("ckpt"):
-        trainer.restoreStates(torch.load(saver.SavePath, {"cuda:0": f"cuda:{rank}"})[Consts.Fingerprint])
+    if tmpFile is not None:
+        saver.info("Found ckpt to resume at %s", resume)
+        trainer.restoreStates(tmpFile)
 
     trainLoader, trainSampler = getTrainLoader(rank, worldSize, config.Dataset, config.BatchSize, logger=saver)
     valLoader = getValLoader(config.ValDataset, config.BatchSize, disable=rank != 0, logger=saver)
