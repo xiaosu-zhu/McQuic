7f395119063f1b62c4c9587ee69707324aa832c2
Xiaosu <xiaosu.zhu@outlook.com>
2022-03-09 20:33:07 +0800

    fix bug

--- mcquic/consts.py
+++ mcquic/consts.py
@@ -10,8 +10,10 @@ class ConstsMetaClass(type):
     @property
     def TempDir(cls):
         if getattr(cls, '_tempDir', None) is None:
-            tempDir = tempfile.mkdtemp()
+            tempDir = os.path.dirname(tempfile.mktemp())
+            tempDir = os.path.join(tempDir, "mcquic")
             cls._tempDir = tempDir
+            os.makedirs(cls._tempDir, exist_ok=True)
             def removeTmp():
                 shutil.rmtree(tempDir, ignore_errors=True)
             atexit.register(removeTmp)
