49684c0d2e1c67390115e6a26f2ca29a8abcfd22
Xiaosu <xiaosu.zhu@outlook.com>
2022-03-10 23:06:34 +0800

    merge rans from CompressAI to this repo

--- setup.py
+++ setup.py
@@ -1,75 +1,40 @@
-"""
-`get_install_command`, `get_install_paths`
-are export from
-https://github.com/jythontools/wheel
-
-"wheel" copyright (c) 2012-2014 Daniel Holth <dholth@fastmail.fm> and
-contributors.
-"""
-import sys
 import os
-import distutils.dist as dist
-import distutils.command.install as install
+from pathlib import Path
+from setuptools import setup
 
-def get_install_command(name):
-    # late binding due to potential monkeypatching
-    d = dist.Distribution({'name':name})
-    i = install.install(d)
-    i.finalize_options()
-    return i
-
-def get_install_paths(name):
-    """
-    Return the (distutils) install paths for the named dist.
-
-    A dict with ('purelib', 'platlib', 'headers', 'scripts', 'data') keys.
-    """
-    paths = {}
-
-    i = get_install_command(name)
-
-    for key in install.SCHEME_KEYS:
-        paths[key] = getattr(i, 'install_' + key)
-
-    # pip uses a similar path as an alternative to the system's (read-only)
-    # include directory:
-    if hasattr(sys, 'real_prefix'):  # virtualenv
-        paths['headers'] = os.path.join(sys.prefix,
-                                        'include',
-                                        'site',
-                                        'python' + sys.version[:3],
-                                        name)
-
-    return paths
-
-from distutils.core import setup
-
-distribution = setup()
+from pybind11.setup_helpers import Pybind11Extension, build_ext
 
 
-# mcquic
-PKG_NAME = distribution.get_option_dict("metadata")["name"][1]
+CWD = Path(__file__).resolve().parent
+PKG_NAME = "mcquic"
+
+# https://github.com/InterDigitalInc/CompressAI/blob/master/setup.py
+# Copyright (c) 2021-2022, InterDigital Communications, Inc
+def get_extensions():
+    ext_dirs = CWD / "third_party/cpp_exts"
+    ext_modules = []
+
+    # Add rANS module
+    rans_lib_dir = CWD / "third_party/ryg_rans"
+
+    extra_compile_args = ["-std=c++17"]
+    extra_compile_args += ["-O3"]
+    ext_modules.append(
+        Pybind11Extension(
+            name=f"{PKG_NAME}.rans",
+            sources=[str(s) for s in ext_dirs.glob("*.cpp")],
+            language="c++",
+            include_dirs=[rans_lib_dir, ext_dirs],
+            extra_compile_args=extra_compile_args,
+        )
+    )
+
+    return ext_modules
 
 
-def parseEntryPoints(entryPoints):
-    result = dict()
-    entries = entryPoints.strip().split("\n")
-    for ent in entries:
-        if len(ent) < 1:
-            continue
-        key, value = ent.strip().split("=")
-        result[key.strip()] = value.strip()
-    return result
-
-
-# {'mcquic': 'mcquic.cli:entryPoint', 'mcquic-train': 'mcquic.train.cli:entryPoint'}
-ENTRY_POINTS = parseEntryPoints(distribution.get_option_dict("options.entry_points")["console_scripts"][1])
-
-
-BIN_PATH = get_install_paths(PKG_NAME)["scripts"]
-
-# Add `-O` (Optimizer mode) arg to shebang
-# if WINDOWS:
-#     targetPY = f"{entry}-script.py"
-# else:
-#     targetPY = f"{entry}"
+distribution = setup(
+    ext_modules = get_extensions(),
+    cmdclass = {
+        "build_ext": build_ext
+    }
+)
