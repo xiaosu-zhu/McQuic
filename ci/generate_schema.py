e6e391dcda782dfcea621c5d4a14b7e8cb904ec2
Xiaosu <xiaosu.zhu@outlook.com>
2022-03-13 21:31:55 +0800

    update actions

--- ci/generate_schema.py
+++ ci/generate_schema.py
@@ -9,7 +9,7 @@ mcquic_config = importlib.util.spec_from_file_location("mcquic.config", sys.argv
 config = importlib.util.module_from_spec(mcquic_config)
 mcquic_config.loader.exec_module(config)
 
-spec = APISpec(title="", version="1.0.0", openapi_version="3.0.2", plugins=[MarshmallowPlugin()])
+spec = APISpec(title="", version="1.0.0", openapi_version="3.1.0", plugins=[MarshmallowPlugin()])
 spec.components.schema("result", schema=config.ConfigSchema)
 
 result = spec.to_dict()["components"]["schemas"]
@@ -18,6 +18,15 @@ result.update({key: value for key, value in copy.deepcopy(result["result"]).item
 
 del result["result"]
 
+result["title"] = "Config schema"
+result["description"] = "The bravo schema for writing a config!"
 
 with open(sys.argv[2], "w") as fp:
     json.dump(result, fp)
+
+with open(sys.argv[2]) as fp:
+    lines = fp.readlines()
+
+with open(sys.argv[2], "w") as fp:
+    for line in lines:
+        fp.write(line.replace("/components/schemas", ""))
