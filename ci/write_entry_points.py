57a9966593313f87d3c1b2a9b57d528e5fa4b9be
Xiaosu <xiaosu.zhu@outlook.com>
2022-03-13 23:03:28 +0800

    fix bug

--- ci/write_entry_points.py
+++ ci/write_entry_points.py
@@ -25,4 +25,5 @@ def writeYAML(lines):
 if __name__ == "__main__":
     with open(sys.argv[1]) as fp:
         result = writeYAML(fp.readlines())
-        sys.stdout.writelines(result)
+    with open(sys.argv[1], "w") as fp:
+        fp.writelines(result)
