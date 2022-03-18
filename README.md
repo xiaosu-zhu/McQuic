2fe3ec1b9c51e5a30fa752a0979044eed0280a4e
Xiaosu <xiaosu.zhu@outlook.com>
2022-03-18 10:26:53 +0800

    update streamlit

--- README.md
+++ README.md
@@ -45,7 +45,7 @@
     <img src="https://raw.githubusercontent.com/xiaosu-zhu/McQuic/main/assets/papertitle-dark.svg#gh-dark-mode-only" alt="Unified Multivariate Gaussian Mixture for Efficient Neural Image Compression" title="Unified Multivariate Gaussian Mixture for Efficient Neural Image Compression" width="100%"/>
   </a>
 </p>
-  <p align="center"><a href="localhost">CVF Open Access</a> | <a href="localhost">arXiv</a> | <a href="#citation">BibTex</a> | Demo will be online soon</p>
+<p align="center"><a href="localhost" target="_blank">CVF Open Access</a> | <a href="localhost" target="_blank">arXiv</a> | <a href="#citation">BibTex</a> | <a href="https://huggingface.co/spaces/xiaosu-zhu/McQuic" target="_blank">Demo</a></p>
 
 
 
--- mcquic/service/cli.py
+++ mcquic/service/cli.py
@@ -7,14 +7,9 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 @click.command(context_settings=CONTEXT_SETTINGS)
 @click.option("-D", "--debug", is_flag=True, help="Set logging level to DEBUG to print verbose messages.")
 @click.option("-q", "--quiet", is_flag=True, help="Silence all messages, this option has higher priority to `-D/--debug`.")
+@click.option("-qp", type=click.IntRange(1, 13), default=3, show_default=True, help="Quantization parameter. Higher means better image quality and larger size.")
 @click.option("-d", "--disable-gpu", is_flag=True, help="Disable GPU usage.")
-def entryPoint(debug, quiet, disable_gpu):
-    """Create training set from `images` dir to `output` dir.
-
-Args:
-
-    images (str): All training images folder, allow sub-folders.
-
-    output (str): Output dir to create training set.
+def entryPoint(debug, quiet, qp, disable_gpu):
+    """Start a Streamlit service for compressing and restoring images
     """
-    main(debug, quiet, disable_gpu)
+    raise NotImplementedError
