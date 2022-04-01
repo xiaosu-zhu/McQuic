e9aae1778967787106fbb981916bc49813f76a33
xiaosu-zhu <xiaosu-zhu@users.noreply.github.com>
2022-04-01 13:22:37 +0000

    Auto update config schema and configs/README.md

--- mcquic/rans.pyi
+++ mcquic/rans.pyi
@@ -24,13 +24,13 @@ class RansDecoder():
         """
         Decode a string to a list of symbols.
 
-        This method is the reverse operation of `RansEncoder.encodeWithIndexes(...)` All args are same.
+        This method is inverse operation of `RansEncoder.encodeWithIndexes(...)`. All args are same.
 
         Args:
-            encoded (str): Encode byte string.
-            indexes (List[int]): Index of CDF and cdfSize of i-th symbol to be used for encode.
-            cdfs (List[List[int]]): A series of CDFs. Each corresponds to a group with specific PMF.
-            cdfSizes (List[int]): Symbol upper-bound for each group.
+            encoded (bytes): Encoded byte string.
+            indexes (List[int]): Index of CDF and cdfSize to pick for i-th symbol.
+            cdfs (List[List[int]]): A series of CDFs. Each corresponds to a different symbol group. Different groups have different CDFs since they are under different distributions.
+            cdfSizes (List[int]): Symbol integer upper-bound for each group.
             offsets (List[int]): Offset applied to each symbol.
 
         Returns:
@@ -46,26 +46,26 @@ class RansEncoder():
         """
         Encode list of symbols to string.
 
-        This method accepts symbols under mixed distributions. Therefore, symbol from different distribution can be encoded by its corresponding cdf to achieve the best rate.
+        This method accepts symbols under mixed distributions. Therefore, symbol from different distribution can be encoded by its corresponding CDF to achieve the best rate.
 
         Args:
-            symbols (List[int]): List of integers ranges in [0, cdfSize[index]] to be encoded.
-            indexes (List[int]): Index of CDF and cdfSize of i-th symbol to be used for encode.
-            cdfs (List[List[int]]): A series of CDFs. Each corresponds to a group with specific PMF.
-            cdfSizes (List[int]): Symbol upper-bound for each group.
+            encoded (bytes): Encoded byte string.
+            indexes (List[int]): Index of CDF and cdfSize to pick for i-th symbol.
+            cdfs (List[List[int]]): A series of CDFs. Each corresponds to a different symbol group. Different groups have different CDFs since they are under different distributions.
+            cdfSizes (List[int]): Symbol integer upper-bound for each group.
             offsets (List[int]): Offset applied to each symbol.
 
         Returns:
-            str: Encoded byte string.
+            bytes: Encoded byte string.
         """
     pass
-def pmfToQuantizedCDF(arg0: typing.List[float], arg1: int) -> typing.List[int]:
+def pmfToQuantizedCDF(pmf: typing.List[float], precision: int) -> typing.List[int]:
     """
     Return quantized CDF for a given PMF with total quantization level `2 ** precision`.
 
     Args:
-        pmf (List[float]): Probability mass function (normalized occuring frequency) for all symbols.
-        precision (int): Total quantization level for CDF (cdfSize).
+        pmf (List[float]): Probability mass function (normalized symbol frequency).
+        precision (int): Total quantization level in bits (`log2(cdfSize)`).
 
     return:
         List[int]: Quantized CDF, length = `2 ** precision`.
