07ac5b349d8c016877b2c36fb9ace6179026a73e
Xiaosu <xiaosu.zhu@outlook.com>
2022-04-15 12:08:50 +0800

    update daily build action

--- ci/pre_build/check_today_has_commit.py
+++ ci/pre_build/check_today_has_commit.py
@@ -3,8 +3,8 @@ import git
 
 repo = git.Repo(".")
 currentTime = datetime.now(timezone.utc)
-# if currentTime - last commit time more than 26 hours, then return error
-if (currentTime - repo.head.commit.committed_datetime).total_seconds() > 26 * 60 * 60:
+# if currentTime - last commit time more than 48 hours, return false
+if (currentTime - repo.head.commit.committed_datetime).total_seconds() > 48 * 60 * 60:
     print("false")
     exit()
 print("true")
