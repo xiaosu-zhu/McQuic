9efc9e5c5b376e47f3ff5065a914fddc697ea8fb
Xiaosu Zhu <xiaosu.zhu@outlook.com>
2024-04-16 10:09:53 +0800

    move to webdataset finish

--- test_wds.py
+++ test_wds.py
@@ -1,35 +1,14 @@
-"""Package of datasets.
-
-Exports:
-    Basic: A Basic dataset that reads all images from a directory.
-    BasicLMDB: A Basic dataset that reads from a LMDB.
-"""
-from typing import Union
-import logging
-import os
-import json
-import glob
-from torch.utils.data import DataLoader, DistributedSampler
-from vlutils.logger import LoggerBase
-from vlutils.saver import StrPath
+import webdataset as wds
 import torch
-
-from .transforms import getTrainingPreprocess, getEvalTransform
-from .dataset import Basic, BasicLMDB
-
 from torchvision.io.image import ImageReadMode, decode_image
 import webdataset as wds
+import glob
+import os
+from torch.utils.data import DataLoader
 
-__all__ = [
-    "Basic",
-    "BasicLMDB",
-]
+from typing import Union
 
-
-class DummyLoader(DataLoader):
-    def __init__(self, **_):
-        return
-
+from mcquic.datasets.transforms import getTrainingPreprocess
 
 def wdsDecode(sample):
 
@@ -45,22 +24,22 @@ def wdsDecode(sample):
         sample = sample[:3]
     return sample
 
-def getTrainLoader(rank: int, worldSize: int, datasetPath: StrPath, batchSize: int, logger: Union[logging.Logger, LoggerBase] = logging.root):
+
+
+def getTrainLoader(datasetPath, batchSize: int):
     allTarGZ = glob.glob(os.path.join(datasetPath, '*.tar.gz'))
     # NOTE: no need to use disbtribued sampler, since shuffle have difference RNG over time and pid.
-    # NOTE: do not call .repeat(), it hangs!
     trainDataset = wds.WebDataset(allTarGZ).shuffle(1000).map(wdsDecode).map(getTrainingPreprocess())
     # trainDataset = BasicLMDB(datasetPath, transform=getTrainingPreprocess())
-    logger.debug("Create training set: %s", trainDataset)
     # trainSampler = DistributedSampler(trainDataset, worldSize, rank)
     # trainLoader = wds.WebLoader(trainDataset, batch_size=batchSize, num_workers=0, pin_memory=True, prefetch_factor=None, persistent_workers=False)
-    # NOTE: we use native dataloader, num_worker=2 is very enough
     trainLoader = DataLoader(trainDataset, batch_size=batchSize, num_workers=2, pin_memory=True, prefetch_factor=2, persistent_workers=True)
     return trainLoader
 
-def getValLoader(datasetPath: StrPath, disable: bool = False, logger: Union[logging.Logger, LoggerBase] = logging.root):
-    if disable:
-        return DummyLoader()
-    valDataset = Basic(datasetPath, transform=getEvalTransform())
-    logger.debug("Create validation set: %s", valDataset)
-    return DataLoader(valDataset, batch_size=1, shuffle=False, num_workers=4, pin_memory=True, drop_last=False)
+
+
+if __name__ == '__main__':
+    loader = getTrainLoader('/mnt/nfs/CMG/mcquic/', 2)
+    for i, image in enumerate(loader):
+        print(i)
+        print(image.shape)
