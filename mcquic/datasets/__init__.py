d19953af953fe5d56e0926d402c5306772b18986
Xiaosu Zhu <xiaosu.zhu@outlook.com>
2024-04-16 15:44:19 +0800

    add multiprocessing webdataset creation

--- mcquic/datasets/__init__.py
+++ mcquic/datasets/__init__.py
@@ -7,12 +7,15 @@ Exports:
 from typing import Union
 import logging
 import os
-import json
+import io
+from PIL import Image
+Image.MAX_IMAGE_PIXELS = None
 import glob
 from torch.utils.data import DataLoader, DistributedSampler
 from vlutils.logger import LoggerBase
 from vlutils.saver import StrPath
 import torch
+from torch.utils.data import default_collate
 
 from .transforms import getTrainingPreprocess, getEvalTransform
 from .dataset import Basic, BasicLMDB
@@ -32,31 +35,31 @@ class DummyLoader(DataLoader):
 
 
 def wdsDecode(sample):
-
-    sample = torch.ByteTensor(torch.ByteStorage.from_buffer(bytearray(sample['jpg'])))
+    sample = Image.open(io.BytesIO(sample['jpg'])).convert('RGB')
+    # sample = torch.ByteTensor(torch.ByteStorage.from_buffer(bytearray(sample['jpg'])))
     # UNCHANGED --- Slightly speedup
     # No need to force RGB. Transforms will handle it.
-    sample = decode_image(sample, ImageReadMode.UNCHANGED)
-    if len(sample.shape) < 3:
-        sample = sample.expand(3, *sample.shape)
-    if sample.shape[0] == 1:
-        sample = sample.repeat((3, 1, 1))
-    elif sample.shape[0] == 4:
-        sample = sample[:3]
+    # sample = decode_image(sample, ImageReadMode.UNCHANGED)
+    # if len(sample.shape) < 3:
+    #     sample = sample.expand(3, *sample.shape)
+    # if sample.shape[0] == 1:
+    #     sample = sample.repeat((3, 1, 1))
+    # elif sample.shape[0] == 4:
+    #     sample = sample[:3]
     return sample
 
 def getTrainLoader(rank: int, worldSize: int, datasetPath: StrPath, batchSize: int, logger: Union[logging.Logger, LoggerBase] = logging.root):
     allTarGZ = glob.glob(os.path.join(datasetPath, '*.tar.gz'))
     # NOTE: no need to use disbtribued sampler, since shuffle have difference RNG over time and pid.
     # NOTE: do not call .repeat(), it hangs!
-    # NOTE: if number of shard < nodes, do not use shard_splitter, it hangs!
-    trainDataset = wds.WebDataset(allTarGZ).shuffle(1000).map(wdsDecode).map(getTrainingPreprocess())
-    # trainDataset = BasicLMDB(datasetPath, transform=getTrainingPreprocess())
+    # NOTE: if number of shard < nodes, do not use shardshuffle, it hangs!
+    # NOTE: shuffle as large as it can to ensure randomness
+    # NOTE: they (wds) recommend to batch in advance, not in dataloader
+    # NOTE: don't use their (wds) collate function, it is wrong.
+    trainDataset = wds.WebDataset(allTarGZ, shardshuffle=None).shuffle(10000).map(wdsDecode).map(getTrainingPreprocess()).batched(batchSize, collation_fn=default_collate)
     logger.debug("Create training set: %s", trainDataset)
-    # trainSampler = DistributedSampler(trainDataset, worldSize, rank)
-    # trainLoader = wds.WebLoader(trainDataset, batch_size=batchSize, num_workers=0, pin_memory=True, prefetch_factor=None, persistent_workers=False)
-    # NOTE: we use native dataloader, num_worker=2 is very enough
-    trainLoader = DataLoader(trainDataset, batch_size=batchSize, num_workers=2, pin_memory=True, prefetch_factor=2, persistent_workers=True)
+    # NOTE: we use native dataloader, num_worker=4 is very enough
+    trainLoader = DataLoader(trainDataset, batch_size=None, num_workers=4, pin_memory=True, prefetch_factor=2, persistent_workers=True)
     return trainLoader
 
 def getValLoader(datasetPath: StrPath, disable: bool = False, logger: Union[logging.Logger, LoggerBase] = logging.root):
