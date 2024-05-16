e5a228e54b04775f54a568fad975d9b661744ad3
Xiaosu Zhu <xiaosu.zhu@outlook.com>
2024-05-16 16:34:58 +0800

    updated quantizer.py

--- multinode.sh
+++ multinode.sh
@@ -19,7 +19,7 @@ source /ssdfs/datahome/tj24011/software/miniconda3/etc/profile.d/conda.sh
 conda activate mcquic
 
 # Graceful restart = 3, for handling data issue
-TOKENIZERS_PARALLELISM=false NCCL_P2P_LEVEL=NVL OMP_NUM_THREADS=8 srun /ssdfs/datahome/tj24011/software/miniconda3/envs/mcquic/bin/torchrun \
+TOKENIZERS_PARALLELISM=false NCCL_P2P_LEVEL=NVL OMP_NUM_THREADS=16 srun torchrun \
 --nnodes 2 \
 --max_restarts 3 \
 --nproc_per_node 8 \
--- singlenode.sh
+++ singlenode.sh
@@ -3,15 +3,19 @@
 #SBATCH -J mcquic_pretraining
 #SBATCH -p A800
 #SBATCH -N 1
-#SBATCH --ntasks=8
-#SBATCH --cpus-per-task=5
-#SBATCH --ntasks-per-node=8
+#SBATCH --ntasks=1
 #SBATCH --gres=gpu:a800:8
+#SBATCH --cpus-per-task=48
 #SBATCH --output=slurm-%j.out
 #SBATCH --error=slurm-%j.err
+
+# ntask should be equal to N
+
+export HF_ENDPOINT="https://hf-mirror.com"
+export PYTHONPATH="/ssdfs/datahome/tj24011/workspace/McQuic"
 
 module load cuda/12.1
 source /ssdfs/datahome/tj24011/software/miniconda3/etc/profile.d/conda.sh
 conda activate mcquic
 
-TOKENIZERS_PARALLELISM=false NCCL_P2P_LEVEL=NVL OMP_NUM_THREADS=8 torchrun --rdzv-backend=c10d --rdzv-endpoint=localhost:0 --nnodes=1 --nproc_per_node=8 /ssdfs/datahome/tj24011/workspace/McQuic/mcquic/train/__main__.py /ssdfs/datahome/tj24011/workspace/McQuic/configs/a800_8.yaml
+TOKENIZERS_PARALLELISM=false NCCL_P2P_LEVEL=NVL OMP_NUM_THREADS=16 srun torchrun --rdzv-backend=c10d --rdzv-endpoint=localhost:0 --nnodes=1 --nproc_per_node=8 mcquic/train/__main__.py configs/a800_8.yaml
