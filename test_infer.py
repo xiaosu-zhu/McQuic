cc64e3cc179370b1f25311160ae50efbd31c3bc6
Xiaosu Zhu <xiaosu.zhu@outlook.com>
2024-05-22 15:54:46 +0800

    updated inference codo

--- test_infer.py
+++ test_infer.py
@@ -18,17 +18,20 @@ def test():
         denseNorm=False,
         qk_norm=True,
         norm_eps=1e-5,
-        loadFrom="/ssdfs/datahome/tj24011/workspace/McQuic/saved/latest/val_30000.ckpt",
-    ).eval().cuda()
+        loadFrom="./compressor/dynamic_30000.ckpt",
+    ).eval()
+    ckpt = torch.load("./generation_saved/latest/saved.ckpt", map_location="cpu")
+    generator.load_state_dict({k.replace("module.", ""): v for k, v in ckpt['trainer']['_model'].items()})
+    generator = generator.cuda()
 
     print(list(name for name, _ in generator.named_parameters() if "gamma" in name))
     with torch.no_grad():
-        class_label = torch.tensor([138, 256]).cuda()
+        class_label = torch.tensor([79, 264]).cuda()
         predictions, restored = generator(None, class_label)
 
     print("****** PRES:", [pre.shape for pre in predictions], "********")
     print(restored.shape)
-    restored_img = restored.detach().cpu().permute(0, 2, 3, 1).numpy().astype("uint8")
+    restored_img = ((restored.detach().cpu().permute(0, 2, 3, 1).numpy() + 1) / 2 * 255). astype("uint8")
     # print(restored_img[0])
     
     im = Image.fromarray(restored_img[0])
